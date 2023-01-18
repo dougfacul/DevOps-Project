b2692d113bdc145da6abdb7ea2aa76e830582d0d
dougfacul <57716416+dougfacul@users.noreply.github.com>
2023-01-18 20:20:43 -0300

    Update backend.tf

--- DevOps_Project_2/Terraform/backend.tf
+++ DevOps_Project_2/Terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "euran-terraform-statefile"
+    bucket = "remotestate-s3-terraform-dst18102022"
     key = "server_name/statefile"
-    region = "ap-south-1"
+    region = "us-east-1"
   }
 }  
